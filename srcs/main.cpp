a30ede6d5c6150a0e9048cae8455e4f637766a9d
CramerRF <victor.alonso.tr@gmail.com>
2024-07-28 11:46:36 +0200

    hello world

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,11 +2,13 @@
 #include <cstdlib>
 #include <exception>
 #include <cstring>
+#include <iostream>
 
 int main (int argc, char **args)
 {
   WebServ *webServ;
 
+  std::cout << "Hello World!" << std::endl;
   try {
     if (argc == 1) webServ = new WebServ(); 
     if (argc == 2) webServ = new WebServ(std::string(args[1])); 
